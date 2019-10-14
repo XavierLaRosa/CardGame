5f273b20cef81b0b545aa3456acc601cc0b8f089
Xavier La Rosa <xavier.a.larosa@gmail.com>
2019-10-14 03:18:30 -0400

    worked on main file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Game x = new Game();
-        x.start();
+        Game newGame = new Game();
+        newGame.start();
     }
 }
